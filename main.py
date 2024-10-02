05cd955b027c49a7a62a612a7d82e2c12079ced1
Simeon <simeon.oosthuizen@gmail.com>
2024-10-02 17:24:48 +0200

    added some code

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-# code starts here
+data = [(1, 'A'), (2, 'A'), (3, 'A'), (6, 'B'), (7, 'B'), (8, 'B')]
+
+threshold = sum(num for num, label in data if label == 'A') / len([label for _, label in data if label == 'A'])
+threshold += sum(num for num, label in data if label == 'B') / len([label for _, label in data if label == 'B'])
+threshold /= 2  
+
+inputnr = float(input("Enter a number: "))
+
+
+if inputnr < threshold:
+    print("Class A")
+else:
+    print("Class B")
