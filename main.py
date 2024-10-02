f2b80dde1832dfc4c19aee84c078feb430772357
Simeon <simeon.oosthuizen@gmail.com>
2024-10-02 17:33:28 +0200

    added shebang and entry point

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+#!/usr/bin/env python3
+
 data = [(1, 'A'), (2, 'A'), (3, 'A'), (6, 'B'), (7, 'B'), (8, 'B')]
 
 threshold = sum(num for num, label in data if label == 'A') / len([label for _, label in data if label == 'A'])
 threshold += sum(num for num, label in data if label == 'B') / len([label for _, label in data if label == 'B'])
 threshold /= 2  
 
-inputnr = float(input("Enter a number: "))
-
-
-if inputnr < threshold:
-    print("Class A")
-else:
-    print("Class B")
+if __name__ == "__main__":
+    inputnr = float(input("Enter a number: "))
+    
+    if inputnr < threshold:
+        print("Class A")
+    else:
+        print("Class B")
